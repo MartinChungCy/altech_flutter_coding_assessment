f69cdefd992cb45cf20d6aaf35fc82e460037f1a
martinchungcy@gmail.com <>
2024-10-31 03:11:11 +0800

    - refactor edit expense dialog

--- lib/edit_expense_dialog.dart
+++ lib/edit_expense_dialog.dart
@@ -1,5 +1,7 @@
+import 'package:dropdown_button2/dropdown_button2.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:intl/intl.dart';
 import 'bloc/expense_bloc.dart';
 import 'bloc/expense_event.dart';
 import 'bloc/expense_item.dart';
@@ -7,6 +9,9 @@ import 'bloc/expense_item.dart';
 class EditExpenseDialog extends StatefulWidget {
   final ExpenseItem expense;
   final int index;
+
+  static DateTime firstDate = DateTime(2020);
+  static DateTime lastDate = DateTime(2100);
 
   const EditExpenseDialog(
       {super.key, required this.expense, required this.index});
@@ -16,14 +21,11 @@ class EditExpenseDialog extends StatefulWidget {
 }
 
 class EditExpenseDialogState extends State<EditExpenseDialog> {
-  static DateTime firstDate = DateTime(2020);
-  static DateTime lastDate = DateTime(2100);
-
   late TextEditingController _titleController;
   late TextEditingController _amountController;
   late String _selectedCategory;
   final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
-  DateTime? _selectedDate;
+  late DateTime _selectedDate;
 
   @override
   void initState() {
@@ -47,8 +49,8 @@ class EditExpenseDialogState extends State<EditExpenseDialog> {
     final DateTime? picked = await showDatePicker(
         context: context,
         initialDate: initialDate,
-        firstDate: firstDate,
-        lastDate: lastDate);
+        firstDate: EditExpenseDialog.firstDate,
+        lastDate: EditExpenseDialog.lastDate);
     if (picked != null && picked != initialDate) {
       setState(() {
         _selectedDate = picked;
@@ -61,11 +63,10 @@ class EditExpenseDialogState extends State<EditExpenseDialog> {
       final String title = _titleController.text;
       final double amount = double.tryParse(_amountController.text) ?? 0.0;
       final ExpenseItem updatedExpense = widget.expense.copyWith(
-        title: title,
-        amount: amount,
-        category: _selectedCategory,
-        date: _selectedDate ?? DateTime.now(),
-      );
+          title: title,
+          amount: amount,
+          category: _selectedCategory,
+          date: _selectedDate ?? DateTime.now());
       context
           .read<ExpenseBloc>()
           .add(UpdateExpenseEvent(widget.index, updatedExpense));
@@ -73,77 +74,34 @@ class EditExpenseDialogState extends State<EditExpenseDialog> {
     }
   }
 
-  String? _validateTitle(String? value) {
-    if (value == null || value.isEmpty) {
-      return 'Please enter a title';
-    }
-    return null;
-  }
+  String? _validateTitle(String? value) =>
+      isEmptyOrNull(value) ? 'Please enter a title' : null;
 
   String? _validateAmount(String? value) {
-    if (value == null || value.isEmpty) {
-      return 'Please enter an amount';
-    }
-    if (double.tryParse(value) == null) {
-      return 'Please enter a valid number';
-    }
+    if (isEmptyOrNull(value)) return 'Please enter an amount';
+    if (double.tryParse(value!) == null) return 'Please enter a valid number';
     return null;
   }
 
-  String? _validateCategory(String? value) {
-    if (value == null || value.isEmpty) {
-      return 'Please select a category';
-    }
-    return null;
-  }
+  String? _validateCategory(String? value) =>
+      isEmptyOrNull(value) ? 'Please select a category' : null;
+
+  bool isEmptyOrNull(String? value) => value == null || value.isEmpty;
 
   @override
   Widget build(BuildContext context) {
     return AlertDialog(
+      scrollable: true,
       title: const Text('Edit Expense'),
       content: Form(
         key: _formKey,
         child: Column(
           mainAxisSize: MainAxisSize.min,
           children: [
-            TextFormField(
-              controller: _titleController,
-              decoration: const InputDecoration(labelText: 'Title'),
-              validator: _validateTitle,
-            ),
-            TextFormField(
-              controller: _amountController,
-              decoration: const InputDecoration(labelText: 'Amount'),
-              keyboardType: TextInputType.number,
-              validator: _validateAmount,
-            ),
-            DropdownButtonFormField<String>(
-              value: _selectedCategory,
-              decoration: const InputDecoration(labelText: 'Category'),
-              items: categories.map((String category) {
-                return DropdownMenuItem<String>(
-                  value: category,
-                  child: Text(category),
-                );
-              }).toList(),
-              onChanged: (newValue) {
-                setState(() {
-                  _selectedCategory = newValue!;
-                });
-              },
-              validator: _validateCategory,
-            ),
-            Row(
-              children: [
-                Text(_selectedDate != null
-                    ? _selectedDate!.toLocal().toString().split(' ')[0]
-                    : 'No date selected'),
-                IconButton(
-                  icon: const Icon(Icons.calendar_today),
-                  onPressed: () => _pickDate(context),
-                ),
-              ],
-            ),
+            _buildTitleField(),
+            _buildAmountField(),
+            _buildCategoryDropdown(),
+            _buildDatePickerRow(context),
           ],
         ),
       ),
@@ -155,6 +113,64 @@ class EditExpenseDialogState extends State<EditExpenseDialog> {
         TextButton(
           onPressed: _updateExpense,
           child: const Text('Save'),
+        ),
+      ],
+    );
+  }
+
+  TextFormField _buildTitleField() {
+    return TextFormField(
+      controller: _titleController,
+      decoration: const InputDecoration(labelText: 'Title'),
+      validator: _validateTitle,
+    );
+  }
+
+  TextFormField _buildAmountField() {
+    return TextFormField(
+      controller: _amountController,
+      decoration: const InputDecoration(labelText: 'Amount'),
+      keyboardType: TextInputType.number,
+      validator: _validateAmount,
+    );
+  }
+
+  DropdownButtonFormField2<String> _buildCategoryDropdown() {
+    return DropdownButtonFormField2<String>(
+      value: _selectedCategory,
+      decoration: const InputDecoration(
+        labelText: 'Select Category',
+      ),
+      dropdownStyleData: DropdownStyleData(
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(15),
+        ),
+      ),
+      items: categories.map((String category) {
+        return DropdownMenuItem<String>(
+          value: category,
+          child: Text(category),
+        );
+      }).toList(),
+      onChanged: (newValue) {
+        setState(() {
+          _selectedCategory = newValue!;
+        });
+      },
+      validator: _validateCategory,
+    );
+  }
+
+  Row _buildDatePickerRow(BuildContext context) {
+    return Row(
+      children: [
+        Text(
+          // _selectedDate!.toLocal().toString().split(' ')[0],
+          DateFormat('dd/MM/yyyy').format(_selectedDate),
+        ),
+        IconButton(
+          icon: const Icon(Icons.calendar_today),
+          onPressed: () => _pickDate(context),
         ),
       ],
     );
